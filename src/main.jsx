4c029291cf6717af7950746e5f118d68100b81f2
jonbo86-oss <jonbo86@gmail.com>
2026-04-27 18:34:25 +0200

    Deploy login protected account flow

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './AppEnhanced.jsx'
+import App from './AppLogin.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
