e6b2249e1f0c72a56834e497d637c661e5848eee
jonbo86-oss <jonbo86@gmail.com>
2026-04-27 18:45:02 +0200

    Deploy fixed login checkout flow

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './AppLogin.jsx'
+import App from './AppLogin2.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
