7a3bf41764d80ff03ff922be3b9b3bd2f649ed8f
jonbo86-oss <jonbo86@gmail.com>
2026-04-27 18:05:34 +0200

    Use stable App7 after blank screen

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App6.jsx'
+import App from './App7.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
