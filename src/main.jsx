43f4e47c861e85ac69ab07782e05af8d27cfc7af
jonbo86-oss <jonbo86@gmail.com>
2026-04-27 19:07:34 +0200

    Deploy Gemini JuanIA integration

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './AppManualCoupon.jsx'
+import App from './AppGeminiShell.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
