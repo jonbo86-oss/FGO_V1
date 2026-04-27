ae31c2ca56413ed12952a7da487d51b220d068ce
jonbo86-oss <jonbo86@gmail.com>
2026-04-27 18:17:49 +0200

    Force redeploy JuanIA input fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './AppFinal.jsx'
+import App from './AppFinal2.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
